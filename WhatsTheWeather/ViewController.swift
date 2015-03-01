218c6fbbbc2ed353fb106a521031ee3c7e6fad99
Rommel Rico <rommeltj@gmail.com>
2015-02-28 19:27:23 -0800

    WhatsTheWeather app

--- WhatsTheWeather/ViewController.swift
+++ WhatsTheWeather/ViewController.swift
@@ -10,9 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userCity: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findWeather(sender: AnyObject) {
+        var city = userCity.text.stringByReplacingOccurrencesOfString(" ", withString: "-")
+        var url = NSURL(string: "http://www.weather-forecast.com/locations/\(city)/forecasts/latest")
+        if (url != nil) {
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: { (data, response, error) -> Void in
+                var urlError = false
+                var weather = ""
+                if (error == nil) {
+                    var urlContent = NSString(data: data, encoding: NSUTF8StringEncoding) as NSString!
+                    var urlContentArray = urlContent.componentsSeparatedByString("<span class=\"phrase\">")
+                    if (urlContentArray.count > 0) {
+                        var weatherArray = urlContentArray[1].componentsSeparatedByString("</span>")
+                        weather = weatherArray[0] as String
+                        weather = weather.stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+                    } else {
+                        urlError = true
+                    }
+                } else {
+                    urlError = true
+                }
+                
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    if (urlError == true) {
+                        self.showError()
+                    } else {
+                        self.resultLabel.text = weather
+                    }
+                })
+            })
+            task.resume()
+        } else {
+            showError()
+        }
+    }
+    
+    func showError() {
+        resultLabel.text = "Was not able to find weather for: \(userCity.text)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        resultLabel.text = ""
     }
 
     override func didReceiveMemoryWarning() {
